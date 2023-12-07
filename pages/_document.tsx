09e3dc270438fbde827515b1b99fcd3b3af0d086
Chloe <gtalk.with.nani@gmail.com>
2023-12-08 06:55:35 +0900

    Create Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='ko-KR'>
-      <Head />
+    <Html lang="ko-KR">
+      <Head>
+        <link color="#141414" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon/favicon.ico" rel="shortcut icon" />
+        <link href="/manifest.json" rel="manifest" />
+        <meta name="application-name" content="Komponent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="Black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Komponent" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#DA3832" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
